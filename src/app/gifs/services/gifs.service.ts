36b44ce3c71162b94de12b11e7bb76b99558477d
GRUPOGMV\sbrg <sergio.bravo.gordo@gmv.com>
2024-01-09 17:17:02 +0100

    first commit

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,62 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+  private _tagsHistory: string[] = [];
+  private serviceUrl:   string = 'https://api.giphy.com/v1/gifs'
+  private apiKey:       string = 'tsKdcN79UWjXFkJo24sp1Dj3dtXEYlg0';
+  public gifList: Gif[] = [];
+
+  constructor(private http:HttpClient) {
+    this.loadLocalStorage();
+    console.log('GifsService Ready');
+  };
+
+  get tagsHistory() {
+    return [...this._tagsHistory];
+  }
+
+  private organizeTagHistory(tag: string) {
+    tag = tag.toLowerCase();
+
+    if(this._tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter(oldTag => oldTag !== tag);
+    }
+
+    this._tagsHistory.unshift(tag);
+    this._tagsHistory = this._tagsHistory.splice(0,10);
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage(): void {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage():void {
+    if(!localStorage.getItem('history')) return;
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+
+    if(this._tagsHistory.length === 0) return;
+    this.searchTag(this._tagsHistory[0]);
+  }
+
+  async searchTag(tag: string):Promise<void> {
+    tag = tag.trim();
+    if(tag.length === 0) return;
+    this.organizeTagHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', 10)
+      .set('q', tag);
+
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+      .subscribe((resp) => {
+        this.gifList = resp.data
+      });
+  }
+}
